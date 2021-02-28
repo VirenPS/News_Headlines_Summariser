f952323101a608dabf58ce02acfca8f6bae73289
VirenPS <viren.samani@hotmail.co.uk>
2021-02-28 19:23:46 +0000

    Keyword Summary complete

--- Testing_Beautiful_Soup_v3.py
+++ Testing_Beautiful_Soup_v3.py
@@ -45,27 +45,39 @@ def articles_html_generator(name, URL, top_n_articles):
 
     return article_body
 
-def keyword_summariser_html(article_list):
-    # print(article_list)
-    # Keyword Summariser
-    title_keyword_list = []
-    for article in top_n_articles:
-        colon_char = article.title.find(':')
-        if colon_char != -1:
-            title_keyword_list.append(article.title[0:colon_char])
-    keyword_counter = collections.Counter(title_keyword_list)
+def title_keyword_summariser_html(news_sites_list, excluded_words_list, top_n_results):
+    article_titles = []
+    for news_site in news_sites_list:
+        article_list_news = article_list(news_site.URL)
+        for i in article_list_news:
+            article_titles.append(i.title)
 
-    # Append html table.
-    keyword_html_summary_table = ''
-    for keyword in keyword_counter:
-        # print(keyword, ': ', keyword_counter[keyword])
+    article_title_keywords = []
+
+    for title in article_titles:
+        a = title.split()
+        for word in a:
+            translator = str.maketrans('', '', string.punctuation + '‘' + '’')
+            article_title_keywords.append(word.translate(translator).lower())
+
+
+    keyword_counter = collections.Counter(article_title_keywords).most_common()
+    refined_keywords_list = [i for i in keyword_counter if i[0] not in excluded_words_list]
+
+    keyword_html_summary_table = '''<table style="width: 106px;" border="1" cellpadding="4">
+    <tbody>'''
+
+    for keyword in refined_keywords_list[0:top_n_results]:
         keyword_html_summary_table += f'''<tr>
-        <td style="width: 62px;">&nbsp;{keyword}</td>
-        <td style="width: 43px;">&nbsp;{keyword_counter[keyword]}</td>
+        <td style="width: 62px;">&nbsp;{keyword[0]}</td>
+        <td style="width: 43px;">&nbsp;{keyword[1]}</td>
         </tr>'''
 
+    keyword_html_summary_table += '''</tbody>
+    </table>'''
+    return keyword_html_summary_table
 
-def write_to_html(article_body):
+def write_to_html(article_body, title_keyword_summariser_html):
     # html_file = open('webpage.html','r') #Read File
     html_file = open('webpage.html','w') #Write to file File
 
@@ -77,16 +89,14 @@ def write_to_html(article_body):
     <p><strong><center>Welcome to Viren's News Headline Summary!</center></strong></p>
 
     {article_body}
-
+    <p><strong><center>Keyword Counter</center></strong></p>
+    {title_keyword_summariser_html}
+    </p>
     </body>
     </html>"""
 
     html_file.write(message)
     html_file.close()
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -100,51 +110,6 @@ if __name__ == "__main__":
     for news_site in news_sites_list:
         article_body += articles_html_generator(news_site.name, news_site.URL, 10)
 
-    article_titles = []
+    title_keyword_summariser_html = title_keyword_summariser_html(news_sites_list, excluded_words_list, 10)
 
-    for news_site in news_sites_list:
-        article_list_news = article_list(news_site.URL)
-        for i in article_list_news:
-            article_titles.append(i.title)
-
-    article_title_keywords = []
-
-    for title in article_titles:
-        # keywords = title.split()
-        a = title.split()
-        for word in a:
-            translator = str.maketrans('', '', string.punctuation + '‘' + '’')
-            article_title_keywords.append(word.translate(translator))
-
-
-    keyword_counter = collections.Counter(article_title_keywords).most_common()
-
-    # print(keyword_counter)
-
-    refined_keywords_list = [i for i in keyword_counter if i[0].lower() not in excluded_words_list]
-
-    print(refined_keywords_list[0:10])
-
-
-    # keyword_counter.remove()
-
-
-
-    # for tuple_pair in refined_list[0:20]:
-    #     print(tuple_pair[0], ': ', tuple_pair[1])
-
-
-    # for keyword in keyword_counter:
-    #     print(keyword, ': ', keyword_counter[keyword])
-
-
-#     write_to_html(article_body)
-
-# Summary Table HTML
-# <table style="width: 106px;" border="1" cellpadding="4">
-#     <tbody>
-#     {keyword_html_summary_table}
-#     </tbody>
-#     </table>
-
-# print(articles_html_generator('http://feeds.skynews.com/feeds/rss/home.xml','Sky',10))
+    write_to_html(article_body, title_keyword_summariser_html)
